4eb3321405d8f36f77c42cf45b4a423c90a7abf5
Aaron Jensen <aaronjensen@gmail.com>
2008-09-02 20:54:20 +0000

    Removing Machine.Specifications.Example from the distribution dir Fixing Machine.Specifications.Example to not use shared assembly info

--- Source/Specifications/Machine.Specifications.Example/Properties/AssemblyInfo.cs
+++ Source/Specifications/Machine.Specifications.Example/Properties/AssemblyInfo.cs
@@ -18,3 +18,21 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("01cb66b4-916d-46d3-a63e-afd05644228e")]
+
+[assembly : AssemblyCompany("Machine Project")]
+[assembly : AssemblyCopyright("Copyright © Machine Project 2008")]
+[assembly : AssemblyTrademark("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly : AssemblyVersion("0.2.0.0")]
+[assembly : AssemblyFileVersion("0.2.0.0")]
