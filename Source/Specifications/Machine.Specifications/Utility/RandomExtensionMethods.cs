484fe9040d3fdca8dc69c6e66a84ae9f5e535df4
Aaron Jensen <aaronjensen@gmail.com>
2008-09-27 11:42:53 -0700

    Adding Each<T> extension method

--- Source/Specifications/Machine.Specifications/Utility/RandomExtensionMethods.cs
+++ Source/Specifications/Machine.Specifications/Utility/RandomExtensionMethods.cs
@@ -7,6 +7,14 @@ namespace Machine.Specifications.Utility
 {
   public static class RandomExtensionMethods
   {
+    public static void Each<T>(this IEnumerable<T> enumerable, Action<T> action)
+    {
+      foreach (var t in enumerable)
+      {
+        action(t);
+      }
+    }
+
     public static void InvokeIfNotNull(this Because because)
     {
       if (because != null)
