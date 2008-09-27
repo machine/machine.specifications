c8f0b4ace43ddfbbb08e472e186f7196a270c8fa
Aaron Jensen <aaronjensen@gmail.com>
2008-09-27 13:10:53 +0000

    Removing evil curly braces.

--- Source/Specifications/Machine.Specifications.Example/BankingSpecs.cs
+++ Source/Specifications/Machine.Specifications.Example/BankingSpecs.cs
@@ -11,19 +11,13 @@ namespace Machine.Specifications.Example
     : with_from_account_and_to_account
   {
     Because of = () =>
-    {
       fromAccount.Transfer(1m, toAccount);
-    };
 
     It should_debit_the_from_account_by_the_amount_transferred = () =>
-    {
       fromAccount.Balance.ShouldEqual(0m);
-    };
 
     It should_credit_the_to_account_by_the_amount_transferred = () =>
-    {
       toAccount.Balance.ShouldEqual(2m);
-    };
   }
 
   [Subject(typeof(Account), "Funds transfer")]
@@ -32,14 +26,10 @@ namespace Machine.Specifications.Example
   {
     static Exception exception;
     Because of =()=>
-    {
       exception = Catch.Exception(()=>fromAccount.Transfer(2m, toAccount));
-    };
 
     It should_not_allow_the_transfer =()=>
-    {
       exception.ShouldBeOfType<Exception>();
-    };
   }
 
   public abstract class with_from_account_and_to_account
