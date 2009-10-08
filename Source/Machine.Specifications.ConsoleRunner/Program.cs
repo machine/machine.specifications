7b3cad25fc4fa474e873380c1791417b02d7e7fd
Barry Woods <barrywoods4@gmail.com>
2009-10-08 10:01:44 +0100

    changed formatting back

--- Source/Machine.Specifications.ConsoleRunner/Program.cs
+++ Source/Machine.Specifications.ConsoleRunner/Program.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Text;
 using Machine.Specifications.ConsoleRunner.Properties;
 using Machine.Specifications.Reporting;
 using Machine.Specifications.Reporting.Integration;
@@ -11,136 +10,139 @@ using Machine.Specifications.Runner.Impl;
 
 namespace Machine.Specifications.ConsoleRunner
 {
-    public class Program
+  public class Program
+  {
+
+    [STAThread]
+    [LoaderOptimization(LoaderOptimization.MultiDomain)]
+    public static void Main(string[] args)
     {
-        private readonly IConsole _console;
+      var program = new Program(new DefaultConsole());
+      ExitCode exitCode = program.Run(args);
 
-        public Program(IConsole console)
+      Environment.Exit((int)exitCode);
+    }
+
+    readonly IConsole _console;
+
+    public Program(IConsole console)
+    {
+      _console = console;
+    }
+
+    public ExitCode Run(string[] arguments)
+    {
+      ExceptionReporter reporter = new ExceptionReporter(_console);
+      
+      Options options = new Options();
+      if (!options.ParseArguments(arguments))
+      {
+        _console.WriteLine(Resources.UsageStatement);
+        return ExitCode.Failure;
+      }
+
+      List<ISpecificationRunListener> listeners = new List<ISpecificationRunListener>();
+
+      ISpecificationRunListener mainListener;
+      if (options.TeamCityIntegration)
+      {
+        mainListener = new TeamCityReporter(_console.WriteLine);
+      }
+      else
+      {
+        mainListener = new RunListener(_console, options.Silent);
+      }
+      
+      try
+      {
+
+        if (!String.IsNullOrEmpty(options.HtmlPath))
         {
-            _console = console;
+          if (IsHtmlPathValid(options.HtmlPath))
+          {
+            listeners.Add(GetHtmlReportListener(options));
+          }
+          else
+          {
+            _console.WriteLine("Invalid html path:" + options.HtmlPath);
+            _console.WriteLine(Resources.UsageStatement);
+            return ExitCode.Failure;
+          }
+          
         }
 
-        [STAThread]
-        [LoaderOptimization(LoaderOptimization.MultiDomain)]
-        public static void Main(string[] args)
+        if (!String.IsNullOrEmpty(options.XmlPath))
         {
-            var program = new Program(new DefaultConsole());
-            ExitCode exitCode = program.Run(args);
-
-            Environment.Exit((int) exitCode);
-        }
-
-        public ExitCode Run(string[] arguments)
-        {
-            var reporter = new ExceptionReporter(_console);
-
-            var options = new Options();
-            if (!options.ParseArguments(arguments))
+            if (IsHtmlPathValid(options.XmlPath))
             {
-                _console.WriteLine(Resources.UsageStatement);
-                return ExitCode.Failure;
-            }
-
-            var listeners = new List<ISpecificationRunListener>();
-
-            ISpecificationRunListener mainListener;
-            if (options.TeamCityIntegration)
-            {
-                mainListener = new TeamCityReporter(_console.WriteLine);
+                listeners.Add(GetXmlReportListener(options));
             }
             else
             {
-                mainListener = new RunListener(_console, options.Silent);
+                _console.WriteLine("Invalid xml path:" + options.XmlPath);
+                _console.WriteLine(Resources.UsageStatement);
+                return ExitCode.Failure;
             }
-
-            try
-            {
-                if (!String.IsNullOrEmpty(options.HtmlPath))
-                {
-                    if (IsHtmlPathValid(options.HtmlPath))
-                    {
-                        listeners.Add(GetHtmlReportListener(options));
-                    }
-                    else
-                    {
-                        _console.WriteLine("Invalid html path:" + options.HtmlPath);
-                        _console.WriteLine(Resources.UsageStatement);
-                        return ExitCode.Failure;
-                    }
-                }
-
-                if (!String.IsNullOrEmpty(options.XmlPath))
-                {
-                    if (IsHtmlPathValid(options.XmlPath))
-                    {
-                        listeners.Add(GetXmlReportListener(options));
-                    }
-                    else
-                    {
-                        _console.WriteLine("Invalid xml path:" + options.XmlPath);
-                        _console.WriteLine(Resources.UsageStatement);
-                        return ExitCode.Failure;
-                    }
-                } 
-
-                listeners.Add(mainListener);
-
-                if (options.AssemblyFiles.Count == 0)
-                {
-                    _console.WriteLine(Resources.UsageStatement);
-                    return ExitCode.Failure;
-                }
-
-                var listener = new AggregateRunListener(listeners);
-
-                ISpecificationRunner specificationRunner = new AppDomainRunner(listener, options.GetRunOptions());
-                var assemblies = new List<Assembly>();
-                foreach (string assemblyName in options.AssemblyFiles)
-                {
-                    if (!File.Exists(assemblyName))
-                    {
-                        throw NewException.MissingAssembly(assemblyName);
-                    }
-
-                    Assembly assembly = Assembly.LoadFrom(assemblyName);
-                    assemblies.Add(assembly);
-                }
-
-                specificationRunner.RunAssemblies(assemblies);
-            }
-            catch (Exception ex)
-            {
-                reporter.ReportException(ex);
-                return ExitCode.Error;
-            }
-
-            if (mainListener is ISpecificationResultProvider)
-            {
-                var errorProvider = (ISpecificationResultProvider) mainListener;
-                if (errorProvider.FailureOccured)
-                {
-                    return ExitCode.Failure;
-                }
-            }
-            return ExitCode.Success;
         }
 
-        private static ISpecificationRunListener GetXmlReportListener(Options options)
+        listeners.Add(mainListener);
+        
+        if (options.AssemblyFiles.Count == 0)
         {
-            var listener = new GenerateXmlReportListener(options.XmlPath, options.ShowTimeInformation);
-
-            return listener;
+          _console.WriteLine(Resources.UsageStatement);
+          return ExitCode.Failure;
         }
 
-        private static bool IsHtmlPathValid(string path)
+        var listener = new AggregateRunListener(listeners);
+        
+        ISpecificationRunner specificationRunner = new AppDomainRunner(listener, options.GetRunOptions());
+        List<Assembly> assemblies = new List<Assembly>();
+        foreach (string assemblyName in options.AssemblyFiles)
         {
-            return Directory.Exists(Path.GetDirectoryName(path));
+          if (!File.Exists(assemblyName))
+          {
+            throw NewException.MissingAssembly(assemblyName);
+          }
+
+          Assembly assembly = Assembly.LoadFrom(assemblyName);
+          assemblies.Add(assembly);
         }
 
-        public ISpecificationRunListener GetHtmlReportListener(Options options)
+        specificationRunner.RunAssemblies(assemblies);
+      }
+      catch(Exception ex)
+      {
+        reporter.ReportException(ex);
+        return ExitCode.Error;
+      }
+
+      if (mainListener is ISpecificationResultProvider)
+      {
+        var errorProvider = (ISpecificationResultProvider) mainListener;
+        if (errorProvider.FailureOccured)
         {
-            return new GenerateHtmlReportRunListener(options.HtmlPath, options.ShowTimeInformation);
+          Console.WriteLine("Generic failure occurred, no idea what this is");
+          return ExitCode.Failure;
         }
-      
+      }
+      return ExitCode.Success;
     }
+
+    private static ISpecificationRunListener GetXmlReportListener(Options options)
+    {
+        var listener = new GenerateXmlReportListener(options.XmlPath, options.ShowTimeInformation);
+
+        return listener;
+    }
+
+    static bool IsHtmlPathValid(string path)
+    {
+      return Directory.Exists(Path.GetDirectoryName(path));
+    }
+
+    public ISpecificationRunListener GetHtmlReportListener(Options options)
+    {
+      return new GenerateHtmlReportRunListener(options.HtmlPath, options.ShowTimeInformation);
+    }
+  }
 }
