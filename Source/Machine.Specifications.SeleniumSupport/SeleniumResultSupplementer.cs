65df7a88daf889b963fbd3c7db497b3ce1ccb765
Aaron Jensen <aaronjensen@gmail.com>
2009-10-19 15:32:02 -0700

    Making the selenium supplementer more resilient

--- Source/Machine.Specifications.SeleniumSupport/SeleniumResultSupplementer.cs
+++ Source/Machine.Specifications.SeleniumSupport/SeleniumResultSupplementer.cs
@@ -14,7 +14,7 @@ namespace Machine.Specifications.SeleniumSupport
 
     public Result SupplementResult(Result result)
     {
-      if (result.Passed)
+      if (result.Passed || result.Status == Status.Ignored)
         return result;
 
       Guid guid = Guid.NewGuid();
@@ -22,23 +22,55 @@ namespace Machine.Specifications.SeleniumSupport
       string screenshotPath = Path.Combine(ImagesPath, guid.ToString() + "-screenshot.png");
       string htmlPath = Path.Combine(ImagesPath, guid.ToString() + ".html");
 
-      Selenium.CaptureEntirePageScreenshot(pageScreenshotPath, "");
-      Selenium.CaptureScreenshot(screenshotPath);
-
       Dictionary<string, string> supplement = new Dictionary<string, string>();
 
-      supplement["text-log"] = Selenium.RetrieveLastRemoteControlLogs();
-
-      using (var writer = new StreamWriter(htmlPath))
+      try
       {
-        writer.Write(Selenium.GetHtmlSource());
+        Selenium.CaptureEntirePageScreenshot(pageScreenshotPath, "");
+        supplement["img-full-page-screenshot"] = pageScreenshotPath;
+      }
+      catch (Exception err)
+      {
+        Console.Error.WriteLine("Problem capturing Selenium img-full-page-screenshot");
+        Console.Error.WriteLine(err);
       }
 
-      supplement["img-full-page-screenshot"] = pageScreenshotPath;
-      supplement["img-screenshot"] = screenshotPath;
-      supplement["html-source"] = htmlPath;  
+      try
+      {
+        Selenium.CaptureScreenshot(screenshotPath);
+        supplement["img-screenshot"] = screenshotPath;
+      }
+      catch (Exception err)
+      {
+        Console.Error.WriteLine("Problem capturing Selenium img-screenshot");
+        Console.Error.WriteLine(err);
+      }
 
-      return Result.Supplement(result, "selenium", supplement);
+      try
+      {
+        supplement["text-log"] = Selenium.RetrieveLastRemoteControlLogs();
+      }
+      catch (Exception err)
+      {
+        Console.Error.WriteLine("Problem capturing Selenium text-log");
+        Console.Error.WriteLine(err);
+      }
+
+      try
+      {
+        using (var writer = new StreamWriter(htmlPath))
+        {
+          writer.Write(Selenium.GetHtmlSource());
+        }
+        supplement["html-source"] = htmlPath;  
+      }
+      catch (Exception err)
+      {
+        Console.Error.WriteLine("Problem capturing Selenium html-source");
+        Console.Error.WriteLine(err);
+      }
+
+      return Result.Supplement(result, "Selenium", supplement);
     }
   }
 }
