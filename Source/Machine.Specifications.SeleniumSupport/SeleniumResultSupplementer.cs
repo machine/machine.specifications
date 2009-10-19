ec0a8fe4fcc655622d95aef9331508c30eadbaa4
Aaron Jensen <aaronjensen@gmail.com>
2009-10-19 12:04:51 -0700

    Adding beginnings of Selenium Support

---
+++ Source/Machine.Specifications.SeleniumSupport/SeleniumResultSupplementer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Selenium;
+
+namespace Machine.Specifications.SeleniumSupport
+{
+  public abstract class SeleniumResultSupplementer : ISupplementSpecificationResults
+  {
+    protected abstract string ImagesPath { get; }
+    protected abstract DefaultSelenium Selenium { get; }
+
+    public Result SupplementResult(Result result)
+    {
+      if (result.Passed)
+        return result;
+
+      Guid guid = Guid.NewGuid();
+      string pageScreenshotPath = Path.Combine(ImagesPath, guid.ToString() + "-full-page-screenshot");
+      string screenshotPath = Path.Combine(ImagesPath, guid.ToString() + "-screenshot");
+
+      Selenium.CaptureEntirePageScreenshot(pageScreenshotPath, "");
+      Selenium.CaptureScreenshot(screenshotPath);
+
+      Dictionary<string, string> supplement = new Dictionary<string, string>();
+
+      supplement["full-page-screenshot"] = pageScreenshotPath;
+      supplement["screenshot"] = screenshotPath;
+      supplement["html"] = Selenium.GetHtmlSource();
+      supplement["log"] = Selenium.RetrieveLastRemoteControlLogs();
+
+      return Result.Supplement(result, "selenium", supplement);
+    }
+  }
+}
