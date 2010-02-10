16ec486cd26323a89be49aabccb5ece14096bce8
Alexander Groß <agross@therightstuff.de>
2010-02-11 07:40:01 +0800

    Signing MSpec's main assembly

--- Source/Machine.Specifications/Properties/AssemblyInfo.cs
+++ Source/Machine.Specifications/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -21,3 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly : Guid("49db994f-04d4-47ff-ade6-0dbda007ee13")]
+
+[assembly: AllowPartiallyTrustedCallers]
