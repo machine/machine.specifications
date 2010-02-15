79bcec12d8fcb21f5080553a95fdad06480a5b7b
Alexander Groß <agross@therightstuff.de>
2010-02-15 14:22:56 +0100

    Building with a MSBuild helper class

---
+++ Tools/Rake/msbuild.rb
@@ -0,0 +1,42 @@
+require 'win32/registry'
+include Win32
+
+class MSBuild
+	def self.compile(attributes)
+		version = attributes.fetch(:version, 'v3.5')
+		projectFile = attributes.fetch(:project).to_absolute
+		userProperties = attributes.fetch(:properties, {})
+		userSwitches = attributes.fetch(:switches, {})
+		
+		frameworkDir = File.join(ENV['windir'].dup, 'Microsoft.NET', 'Framework', version)
+		
+		Registry::HKEY_LOCAL_MACHINE.open("SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\#{version}") do |reg|
+			frameworkDir = reg['InstallPath', Win32::Registry::REG_SZ]
+		end
+		
+		msbuild = File.join(frameworkDir, 'msbuild.exe')
+		
+		properties = {
+			:BuildInParallel => false,
+			:BuildRunner => 'Rake',
+			:Configuration => 'Debug'
+		}
+		properties.merge!(userProperties)
+		
+		properties = properties.collect { |key, value|
+			"/property:#{key}=#{value}"
+		}.join " "
+		
+		switches = {
+			:maxcpucount => true,
+			:target => 'Build'
+		}
+		switches.merge!(userSwitches)
+		
+		switches = switches.collect { |key, value|
+			"/#{key}#{":#{value}" unless value.kind_of? TrueClass or value.kind_of? FalseClass}" if value
+		}.join " "
+		
+		sh "#{msbuild.escape} #{projectFile.escape} #{switches} #{properties}"
+	end
+end
