bd773ef89e53c0ba3c2db82606f747506dcc1a16
smaclell <s.j.maclell@gmail.com>
2010-03-04 01:54:01 -0500

    Updated NUnit to 2.5.5 from 2.4.6. This required removing the dependency on Machine.Testing (it requried 2.4.6) This was accomplished by adding the files from Machine.Testing that are used into the project, the TestsFor class and the AutoMocking folder.
    Signed-off-by: smaclell <s.j.maclell@gmail.com>

---
+++ Source/Machine.Specifications.Tests/Machine.Testing/AutoMocking/AutoMockingContainer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Machine.Container;
+using Machine.Container.Model;
+using Machine.Container.Services;
+using Machine.Container.Services.Impl;
+
+using Rhino.Mocks;
+
+namespace Machine.Testing.AutoMocking
+{
+    public class AutoMockingContainer : MachineContainer
+    {
+        private readonly MockingDependencyResolver _mockingDependencyResolver;
+
+        public AutoMockingContainer(MockRepository mocks)
+            : this(new MockingDependencyResolver(mocks))
+        {
+        }
+
+        public AutoMockingContainer(MockingDependencyResolver mockingDependencyResolver)
+            : base(new CompartmentalizedMachineContainer(new MockingDependencyResolverFactory(mockingDependencyResolver)))
+        {
+            _mockingDependencyResolver = mockingDependencyResolver;
+        }
+
+        public virtual TService Get<TService>()
+        {
+            return _mockingDependencyResolver.Get<TService>();
+        }
+    }
+    public class MockingDependencyResolverFactory : DefaultContainerInfrastructureFactory
+    {
+        private readonly MockingDependencyResolver _mockingDependencyResolver;
+
+        public MockingDependencyResolverFactory(MockingDependencyResolver mockingDependencyResolver)
+        {
+            _mockingDependencyResolver = mockingDependencyResolver;
+        }
+
+        #region IContainerInfrastructureFactory Members
+        public override IRootActivatorResolver CreateDependencyResolver()
+        {
+            IRootActivatorResolver resolver = base.CreateDependencyResolver();
+            resolver.AddAfter(typeof(ActivatorStoreActivatorResolver), _mockingDependencyResolver);
+            return resolver;
+        }
+        #endregion
+    }
+}
