77c38d92dfe512ffa43e9dc69d45f091a2ec3bcc
smaclell <s.j.maclell@gmail.com>
2010-03-10 01:58:06 -0500

    Added the /r:Local option to the gallio integration test batch file. This makes it run substantially faster.

--- Tools/Gallio/v3.1.397/TestGallioIntegration.bat
+++ Tools/Gallio/v3.1.397/TestGallioIntegration.bat
@@ -1,3 +1,3 @@
 set buildDir=..\..\..\Build\Debug
-Gallio.Echo %buildDir%\Gallio3.1\Machine.Specifications.TestGallioAdapter.3.1.Tests.dll /pd:%buildDir%\Gallio3.1\
+Gallio.Echo %buildDir%\Gallio3.1\Machine.Specifications.TestGallioAdapter.3.1.Tests.dll /pd:%buildDir%\Gallio3.1\ /r:Local
 rem pause
