5c9cfbc6e07a9954de2af0d8563219bf0b654b29
Alexander Groß <agross@therightstuff.de>
2010-04-05 15:07:27 +0200

    Moving Gallio runner and test assembly output folders to a better location

--- Tools/Gallio/v3.1.397/TestGallioIntegration.bat
+++ Tools/Gallio/v3.1.397/TestGallioIntegration.bat
@@ -1,3 +1,8 @@
+@echo off
 set buildDir=..\..\..\Build\Debug
-Gallio.Echo %buildDir%\Gallio3.1\Machine.Specifications.TestGallioAdapter.3.1.Tests.dll /pd:%buildDir%\Gallio3.1\ /r:Local
-rem pause
+
+echo *** Executing Gallio runner tests. ***
+Gallio.Echo %buildDir%\Tests\Gallio\Machine.Specifications.TestGallioAdapter.3.1.Tests.dll /plugin-directory:%buildDir% /r:Local
+
+echo *** Executing MSpec examples with the Gallio runnner. IT IS OK FOR THIS TO FAIL. ***
+Gallio.Echo %buildDir%\Tests\Gallio\Machine.Specifications.TestGallioAdapter.3.1.TestResources.dll /plugin-directory:%buildDir% /r:Local
