efaf17da79d63373848eb5642065a08782bd7613
Alexander Groß <agross@therightstuff.de>
2010-04-13 21:25:35 +0200

    Running tests with the 4.0 CLR when we build for .NET 4.0, and the 2.0 CLR/framework 3.5 when we build for .NET 3.5

--- Tools/Rake/nunit.rb
+++ Tools/Rake/nunit.rb
@@ -6,9 +6,10 @@ class NUnitRunner
 	def initialize(paths)
 		@resultsDir = paths.fetch(:results, 'results')
 		@compilePlatform = paths.fetch(:platform, 'x86')
+		@clrVersion = paths.fetch(:clr_version, 'net-3.5')
 		
 		if ENV["teamcity.dotnet.nunitlauncher"] # check if we are running in TeamCity
-			# We are not using the TeamCity nunit launcher. We use NUnit with the TeamCity NUnit Addin which needs tO be copied to our NUnit addins folder
+			# We are not using the TeamCity nunit launcher. We use NUnit with the TeamCity NUnit Addin which needs to be copied to our NUnit addins folder
 			# http://blogs.jetbrains.com/teamcity/2008/07/28/unfolding-teamcity-addin-for-nunit-secrets/
 			# The teamcity.dotnet.nunitaddin environment variable is not available until TeamCity 4.0, so we hardcode it for now
 			@teamCityAddinPath = ENV["teamcity.dotnet.nunitaddin"] ? ENV["teamcity.dotnet.nunitaddin"] : 'c:/TeamCity/buildAgent/plugins/dotnetPlugin/bin/JetBrains.TeamCity.NUnitAddin-NUnit'
@@ -22,7 +23,7 @@ class NUnitRunner
 		Dir.mkdir @resultsDir unless exists?(@resultsDir)
 		
 		assemblies.each do |assem|
-			sh "#{@nunitExe} #{assem.escape} /nothread /xml=\"#{@resultsDir}/#{assem.name}.xml\""
+			sh "#{@nunitExe} #{assem.escape} /nothread /xml=\"#{@resultsDir}/#{assem.name}.xml\" /framework=#{@clrVersion}"
 		end
 	end
 end
