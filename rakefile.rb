42ec206c8e3e224a2cceea73bbff77fb9f81fef0
Alexander Groß <agross@therightstuff.de>
2010-05-13 12:36:41 +0200

    Moving the TestDriven.Net and ReSharper installer batches to their respective project directories

--- rakefile.rb
+++ rakefile.rb
@@ -35,7 +35,6 @@ task :configure do
       :out_dir => "Build/#{target}/",
       :package_name => "Distribution/#{project}-net-4.0-#{target}.zip",
       :nunit_framework => "net-3.5",
-      :exclude_from_package => "InstallResharperRunner.4*.*",
       :additional_specs => ["Machine.Specifications.Example.Clr4.dll"],
     }
   }
@@ -115,22 +114,17 @@ task :package => [ "rebuild", "tests:run", "specs:run" ] do
   rm_f configatron.package_name
   
   cp 'License.txt', configatron.out_dir
-  cp_r 'Distribution/Specifications/.', configatron.out_dir
   
   sz = SevenZip.new \
     :tool => 'Tools/7-Zip/7za.exe',
     :zip_name => configatron.package_name
 
-  files = FileList.new('**/*') \
+  Dir.chdir(configatron.out_dir) do
+    sz.zip :files => FileList.new('**/*') \
       .exclude('*.InstallLog') \
       .exclude('*.InstallState') \
       .exclude('Generation') \
-      .exclude('Tests') \
-  
-  files.exclude(configatron.exclude_from_package) if configatron.exists?(:exclude_from_package)
-
-  Dir.chdir(configatron.out_dir) do
-    sz.zip :files => files
+      .exclude('Tests')
   end
 end
 
