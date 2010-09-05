4e5496c035e026a16c3b530130f22bfc38aacad6
Alexander Groß <agross@therightstuff.de>
2010-09-05 19:02:41 +0800

    Reformatting code to 2-space indents

--- Source/Machine.Specifications/ExtensionMethods.cs
+++ Source/Machine.Specifications/ExtensionMethods.cs
@@ -318,16 +318,16 @@ does contain: {2}", items.EachToUsefulString(), list.EachToUsefulString(), conta
       return arg1;
     }
 
-      public static IComparable ShouldBeGreaterThanOrEqualTo(this IComparable arg1, IComparable arg2)
-      {
-          if (arg2 == null) throw new ArgumentNullException("arg2");
-          if (arg1 == null) throw NewException("Should be greater than or equal to {0} but is [null]", arg2);
+    public static IComparable ShouldBeGreaterThanOrEqualTo(this IComparable arg1, IComparable arg2)
+    {
+      if (arg2 == null) throw new ArgumentNullException("arg2");
+      if (arg1 == null) throw NewException("Should be greater than or equal to {0} but is [null]", arg2);
 
-          if (arg1.CompareTo(arg2.TryToChangeType(arg1.GetType())) < 0)
-              throw NewException("Should be greater than or equal to {0} but is {1}", arg2, arg1);
+      if (arg1.CompareTo(arg2.TryToChangeType(arg1.GetType())) < 0)
+        throw NewException("Should be greater than or equal to {0} but is {1}", arg2, arg1);
 
-          return arg1;
-      }
+      return arg1;
+    }
 
     private static object TryToChangeType(this object original, Type type)
     {
@@ -352,16 +352,16 @@ does contain: {2}", items.EachToUsefulString(), list.EachToUsefulString(), conta
       return arg1;
     }
 
-      public static IComparable ShouldBeLessThanOrEqualTo(this IComparable arg1, IComparable arg2)
-      {
-          if (arg2 == null) throw new ArgumentNullException("arg2");
-          if (arg1 == null) throw NewException("Should be less than or equal to {0} but is [null]", arg2);
+    public static IComparable ShouldBeLessThanOrEqualTo(this IComparable arg1, IComparable arg2)
+    {
+      if (arg2 == null) throw new ArgumentNullException("arg2");
+      if (arg1 == null) throw NewException("Should be less than or equal to {0} but is [null]", arg2);
 
-          if (arg1.CompareTo(arg2.TryToChangeType(arg1.GetType())) > 0)
-              throw NewException("Should be less than or equal to {0} but is {1}", arg2, arg1);
+      if (arg1.CompareTo(arg2.TryToChangeType(arg1.GetType())) > 0)
+        throw NewException("Should be less than or equal to {0} but is {1}", arg2, arg1);
 
-          return arg1;
-      }
+      return arg1;
+    }
 
     public static void ShouldBeCloseTo(this float actual, float expected)
     {
