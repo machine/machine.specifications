f2961c44d98065b792650ac537fe04a0bed7c28d
Alexander Groß <agross@therightstuff.de>
2011-03-05 20:43:47 +0100

    Splitting assembly info and version info

--- Source/SharedAssemblyInfo.cs
+++ Source/SharedAssemblyInfo.cs
@@ -2,19 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Machine Project")]
 [assembly: AssemblyProduct("Machine.Specifications")]
-[assembly: AssemblyCopyright("Copyright © Machine Project 2008, 2009, 2010")]
+[assembly: AssemblyCopyright("Copyright © Machine Project 2008, 2009, 2010, 2011")]
 [assembly: AssemblyTrademark("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
