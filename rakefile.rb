35c95002205fe93b83d06076e875f37c6d682ebd
Alexander Groß <agross@therightstuff.de>
2011-03-06 12:38:04 +0100

    Re-enabling Gallio test run

--- rakefile.rb
+++ rakefile.rb
@@ -167,8 +167,7 @@ namespace :tests do
   
   task :run do
     puts 'Running Gallio tests...'
-    # This fails left and right for no obvious reason.
-    # sh "Tools/Gallio/v3.1.397/Gallio.Echo.exe", "#{configatron.out_dir}/Tests/Gallio/Machine.Specifications.TestGallioAdapter.3.1.Tests.dll", "/plugin-directory:#{configatron.out_dir}", "/r:Local"
+    sh "Tools/Gallio/v3.1.397/Gallio.Echo.exe", "#{configatron.out_dir}/Tests/Gallio/Machine.Specifications.TestGallioAdapter.3.1.Tests.dll", "/plugin-directory:#{configatron.out_dir}", "/r:Local"
   end
 end
 
