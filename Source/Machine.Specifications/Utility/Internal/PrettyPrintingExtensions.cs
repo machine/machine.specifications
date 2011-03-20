6b79a10e1788d8af7c3b43232a5ef5069f81811c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-03-20 10:22:13 +0100

    Closes #36. Improve reporting of unequal strings

--- Source/Machine.Specifications/Utility/Internal/PrettyPrintingExtensions.cs
+++ Source/Machine.Specifications/Utility/Internal/PrettyPrintingExtensions.cs
@@ -8,6 +8,125 @@ namespace Machine.Specifications.Utility.Internal
 {
   public static class PrettyPrintingExtensions
   {
+    public static string FormatErrorMessage(object actualObject, object expectedObject)
+    {
+      if (actualObject == null || !(actualObject is string) ||
+          expectedObject == null || !(expectedObject is string))
+      {
+        // format objects
+        var actual = actualObject.ToUsefulString();
+        var expected = expectedObject.ToUsefulString();
+        return string.Format("  Expected: {1}{0}  But was:  {2}", Environment.NewLine, expected, actual);
+      }
+      else
+      {
+        // format strings
+        var actual = actualObject as string;
+        var expected = expectedObject as string;
+
+        var expectedStringLengthMessage = GetExpectedStringLengthMessage(expected.Length, actual.Length);
+        var firstIndexOfFirstDifference = GetFirstIndexOfFirstDifference(actual, expected);
+        string actualReported;
+        string expectedReported;
+        GetStringsAroundFirstDifference(expected, actual, firstIndexOfFirstDifference, out actualReported, out expectedReported);
+
+        var count = GetFirstIndexOfFirstDifference(actualReported, expectedReported);
+
+        return string.Format(
+                  "  {1} Strings differ at index {2}.{0}" +
+                  "  Expected: \"{3}\"{0}" +
+                  "  But was:  \"{4}\"{0}" +
+                  "  -----------{5}^",
+                  Environment.NewLine,
+                  expectedStringLengthMessage,
+                  firstIndexOfFirstDifference,
+                  expectedReported,
+                  actualReported,
+                  new string('-', count));
+      }
+    }
+
+    static void GetStringsAroundFirstDifference(string expected, string actual, int firstIndexOfFirstDifference, out string actualReported, out string expectedReported)
+    {
+      var left = firstIndexOfFirstDifference;
+      var actualRight = firstIndexOfFirstDifference;
+      var expectedRight = firstIndexOfFirstDifference;
+      var keepAugmenting = true;
+
+      while (keepAugmenting &&
+          IsInCopyFrameLength(left, actualRight, actual.Length) &&
+          IsInCopyFrameLength(left, expectedRight, expected.Length))
+      {
+        keepAugmenting = false;
+
+        if (left > 0)
+        {
+          left--;
+          keepAugmenting = true;
+        }
+        if (IsInCopyFrameLength(left, actualRight, actual.Length) &&
+            IsInCopyFrameLength(left, expectedRight, expected.Length))
+        {
+          if (actual.Length > actualRight)
+          {
+            actualRight++;
+            keepAugmenting = true;
+          }
+          if (expected.Length > expectedRight)
+          {
+            expectedRight++;
+            keepAugmenting = true;
+          }
+        }
+      }
+
+      actualReported = actual.Substring(left, actualRight - left);
+      expectedReported = expected.Substring(left, expectedRight - left);
+
+      if (left != 0)
+      {
+        actualReported = "..." + actualReported;
+        expectedReported = "..." + expectedReported;
+      }
+      if (actualRight != actual.Length || expectedRight != expected.Length)
+      {
+        actualReported = actualReported + "...";
+        expectedReported = expectedReported + "...";
+      }
+    }
+
+    static bool IsInCopyFrameLength(int start, int end, int max)
+    {
+      int length = end - start;
+      if (start > 0)
+        length += 3;
+      if (end < max)
+        length += 3;
+      return length < 64;
+    }
+
+    static int GetFirstIndexOfFirstDifference(string actual, string expected)
+    {
+      for (var i = 0; i < actual.Length; i++)
+      {
+        if (expected.Length < i || expected[i] != actual[i])
+        {
+          return i;
+        }
+      }
+      return actual.Length;
+    }
+
+    static string GetExpectedStringLengthMessage(int actual, int expected)
+    {
+      if (actual == expected)
+      {
+        return string.Format("String lengths are both {0}.", actual);
+      }
+
+      return string.Format("Expected string length {0} but was {1}.", actual, expected);
+    }
+    
     static string Tab(this string str)
     {
       if (string.IsNullOrEmpty(str))
