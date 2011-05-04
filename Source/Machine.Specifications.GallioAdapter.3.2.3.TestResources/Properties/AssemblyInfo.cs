414e2221b4f8efec81a3b2348c52a547688e8563
Jason Gill <jg_git@thegillhousehold.com>
2011-05-04 14:19:20 -0500

    Updated Gallio Adapter for v3.2.3.  Not sure why the unit tests are failing.

---
+++ Source/Machine.Specifications.GallioAdapter.3.2.3.TestResources/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "Machine.Specifications.GallioAdapter.3.2.3.TestResources" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Machine.Specifications.GallioAdapter.3.2.3.TestResources" )]
+[assembly: AssemblyCopyright( "Copyright ©  2011" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "edb760f0-e46f-46fa-955f-c3a9938f6fde" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
---
+++ Source/Machine.Specifications.GallioAdapter.3.2.3/Services/GallioRunListener.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications.Runner;
+using Machine.Specifications.Runner.Impl;
+using Machine.Specifications.GallioAdapter.Model;
+using Gallio.Runtime.ProgressMonitoring;
+using Gallio.Model;
+using Gallio.Model.Commands;
+using Gallio.Framework;
+using Gallio.Model.Contexts;
+
+namespace Machine.Specifications.GallioAdapter.Services
+{
+  public class GallioRunListener : RunListenerBase
+  {
+    ISpecificationRunListener _listener;
+    IProgressMonitor _progressMonitor;
+    ITestContext _testContext;
+
+    Dictionary<string, ITestCommand> _commandsBySpec;
+    Dictionary<string, ITestContext> _contextsBySpec;
+
+    TestOutcome _outcome;
+    public TestOutcome Outcome { get { return _outcome; } }
+
+    public GallioRunListener(ISpecificationRunListener listener, IProgressMonitor progressMonitor, 
+      ITestContext context, IEnumerable<ITestCommand> specificationCommands)     
+    {
+      _listener = listener;
+      _progressMonitor = progressMonitor;
+      _testContext = context;
+
+      _outcome = TestOutcome.Passed;
+
+      _contextsBySpec = new Dictionary<string, ITestContext>();      
+      _commandsBySpec = (from command in specificationCommands
+                         let specificationTest = command.Test as MachineSpecificationTest
+                         where specificationTest != null
+                         select new { Name = specificationTest.Specification.Name, Command = command })
+                         .ToDictionary(x => x.Name, x => x.Command);
+    }
+
+    public override void OnContextStart(ContextInfo context)
+    {
+      _listener.OnContextStart(context);
+      _testContext.LifecyclePhase = LifecyclePhases.Starting;
+      _progressMonitor.SetStatus(context.FullName);   
+    }
+
+    public override void OnContextEnd(ContextInfo context)
+    {
+      _listener.OnContextEnd(context);
+      _testContext.LifecyclePhase = LifecyclePhases.Finishing;      
+    }
+
+    public override void OnSpecificationStart(SpecificationInfo specification)
+    {
+      _listener.OnSpecificationStart(specification);
+
+      ITestCommand specCommand = _commandsBySpec[specification.Name];
+      ITestContext specContext =  specCommand.StartPrimaryChildStep(_testContext.TestStep);
+      _contextsBySpec.Add(specification.Name, specContext);
+
+      specContext.LifecyclePhase = LifecyclePhases.Starting;
+      _progressMonitor.SetStatus("» " + specification.Name);       
+    }
+
+    public override void OnSpecificationEnd(SpecificationInfo specification, Result result)
+    {
+      _listener.OnSpecificationEnd(specification, result);
+
+      ITestContext testContext = _contextsBySpec[specification.Name];
+      testContext.LifecyclePhase = LifecyclePhases.Finishing;
+
+      TestOutcome outcome;
+      TimeSpan? span = null;
+
+      if (result.Status == Status.NotImplemented)
+      {
+        TestLog.Warnings.WriteLine("{0} ({1})", specification.Name, "NOT IMPLEMENTED");
+        TestLog.Warnings.Flush();
+
+        outcome = TestOutcome.Pending;
+        span = new TimeSpan(0);
+      }
+      else if (result.Status == Status.Ignored)
+      {
+        TestLog.Warnings.WriteLine("{0} ({1})", specification.Name, "IGNORED");
+        TestLog.Warnings.Flush();
+
+        outcome = TestOutcome.Ignored;
+        span = new TimeSpan(0);        
+      }
+      else if (result.Passed)
+      {
+        outcome = TestOutcome.Passed;
+      }
+      else
+      {        
+        TestLog.Failures.WriteException( Convert( result.Exception));
+        TestLog.Failures.Flush();
+
+        outcome = TestOutcome.Failed;
+      }
+       
+      testContext.FinishStep(outcome, span);
+      _outcome = _outcome.CombineWith(outcome);
+
+      _progressMonitor.Worked(1);
+    }
+
+    public override void OnFatalError(ExceptionResult exception)
+    {
+      _listener.OnFatalError(exception);
+    }
+
+    Gallio.Common.Diagnostics.ExceptionData Convert(ExceptionResult result)
+    {
+      if (result == null) 
+        return null;
+
+      Gallio.Common.Diagnostics.ExceptionData inner = Convert(result.InnerExceptionResult);
+      return new Gallio.Common.Diagnostics.ExceptionData(result.TypeName, result.Message, result.StackTrace, null, inner);
+    }
+  }
+}
