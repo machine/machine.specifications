c22bf5a0635a65523114ea17516808738a525cd4
Hadi Hariri <mail@hadihariri.com>
2011-07-19 17:58:33 +0200

    ReSharper 6 Support Mostly Complete. Pending Test

---
+++ Source/Machine.Specifications.ReSharperRunner.6.0/Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml;
+
+using JetBrains.ReSharper.UnitTestFramework;
+
+namespace Machine.Specifications.ReSharperRunner
+{
+    public interface ISerializableUnitTestElement
+    {
+        void WriteToXml(XmlElement parent);
+        IUnitTestElement ReadFromXml(XmlElement parent, IUnitTestElement parentElement, MSpecUnitTestProvider provider);
+    }
+}
