aaa6f0390b410c69735418371af9b5290a49a7f0
Daniel Schilling <mail@daniel-schilling.de>
2011-08-31 14:31:10 +0200

    Deleting commented out lines

--- Source/Machine.Specifications/Runner/SpecificationInfo.cs
+++ Source/Machine.Specifications/Runner/SpecificationInfo.cs
@@ -18,35 +18,5 @@ namespace Machine.Specifications.Runner
       ContainingType = containingType;
       FieldName = fieldName;
     }
-
-    //public override bool Equals(object obj)
-    //{
-    //    if (ReferenceEquals(null, obj))
-    //    {
-    //        return false;
-    //    }
-    //    if (ReferenceEquals(this, obj))
-    //    {
-    //        return true;
-    //    }
-    //    if (obj.GetType() != typeof(SpecificationInfo))
-    //    {
-    //        return false;
-    //    }
-    //    return GetHashCode() == obj.GetHashCode();
-    //}
-
-    //public override int GetHashCode()
-    //{
-    //    unchecked
-    //    {
-    //        int hash = 17;
-    //        hash = hash * 29 + (Name != null ? Name.GetHashCode() : 0);
-    //        hash = hash * 29 + (ContainingType != null ? ContainingType.GetHashCode() : 0);
-    //        hash = hash * 29 + (FieldName != null ? FieldName.GetHashCode() : 0);
-
-    //        return hash;
-    //    }
-    //}
   }
 }
