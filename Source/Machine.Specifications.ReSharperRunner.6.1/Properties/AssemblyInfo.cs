dcfb24b6174fc43554373f4f9fec0bbe4377d8b1
Victor Kropp <victor.kropp@gmail.com>
2011-11-02 13:01:16 +0100

    Project for ReSharper 6.1 test runner created

---
+++ Source/Machine.Specifications.ReSharperRunner.6.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+using JetBrains.Application.PluginSupport;
+using JetBrains.UI.Application.PluginSupport;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Machine.Specifications.ReSharperRunner.6.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("a0468d17-75ce-44fd-a346-534cacd98ecc")]
+[assembly: PluginTitle("Machine.Specifications Runner for ReSharper 6.1")]
+[assembly: PluginDescription("Allows ReSharper 6.1 to run Machine.Specifications as unit tests")]
+[assembly: PluginVendor("Machine")]
