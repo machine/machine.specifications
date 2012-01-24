292e039d6e772baf5087ba89d0ab5569868a9795
Niels Hanberg <nielshanberg@gmail.com>
2012-01-24 13:25:06 +0100

    Remove old gallio version

--- Tools/Gallio/v3.1.397/TestGallioIntegration.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set buildDir=..\..\..\Build\Debug
-
-echo *** Executing Gallio runner tests. ***
-Gallio.Echo %buildDir%\Tests\Gallio\Machine.Specifications.TestGallioAdapter.3.1.Tests.dll /plugin-directory:%buildDir% /r:Local
-
-echo *** Executing MSpec examples with the Gallio runnner. IT IS OK FOR THIS TO FAIL. ***
-Gallio.Echo %buildDir%\Tests\Gallio\Machine.Specifications.TestGallioAdapter.3.1.TestResources.dll /plugin-directory:%buildDir% /r:Local
