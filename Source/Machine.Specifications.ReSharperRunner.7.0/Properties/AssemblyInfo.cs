375350774f304e0423e69e5372328ebde8ab7cfe
Alexander Groß <agross@therightstuff.de>
2012-03-05 22:23:09 +0100

    Correcting ReSharper runner metadata, removed some unneeded resources

--- Source/Machine.Specifications.ReSharperRunner.7.0/Properties/AssemblyInfo.cs
+++ Source/Machine.Specifications.ReSharperRunner.7.0/Properties/AssemblyInfo.cs
@@ -2,13 +2,12 @@
 using System.Runtime.InteropServices;
 
 using JetBrains.Application.PluginSupport;
-using JetBrains.UI.Application.PluginSupport;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Machine.Specifications.ReSharperRunner.6.1")]
+[assembly: AssemblyTitle("Machine.Specifications.ReSharperRunner.7.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
@@ -22,6 +21,6 @@ using JetBrains.UI.Application.PluginSupport;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("a0468d17-75ce-44fd-a346-534cacd98ecc")]
-[assembly: PluginTitle("Machine.Specifications Runner for ReSharper 6.1")]
-[assembly: PluginDescription("Allows ReSharper 6.1 to run Machine.Specifications as unit tests")]
+[assembly: PluginTitle("Machine.Specifications Runner for ReSharper 7.0")]
+[assembly: PluginDescription("Allows ReSharper 7.0 to run Machine.Specifications as unit tests")]
 [assembly: PluginVendor("Machine")]
