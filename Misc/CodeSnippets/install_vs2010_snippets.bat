07538aeca95633df4f916ba3d973fdb9b9ac2c1f
Rainer Schuster <rainer.schuster@clondiag.com>
2012-05-10 23:49:34 +0200

    Created Code Snippet files for Visual Studio 2010 and the install scripts. see ./Misc/readme.txt for details
    Closes #104

---
+++ Misc/CodeSnippets/install_vs2010_snippets.bat
@@ -0,0 +1,13 @@
+
+mkdir "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets" 2> NUL
+
+copy "snippets\bec.snippet" "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets"
+copy "snippets\becex.snippet" "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets"
+copy "snippets\espec.snippet" "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets"
+copy "snippets\est.snippet" "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets"
+copy "snippets\fail.snippet" "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets"
+copy "snippets\it.snippet" "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets"
+copy "snippets\l.snippet" "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets"
+copy "snippets\ll.snippet" "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets"
+copy "snippets\spec.snippet" "%USERPROFILE%\Documents\Visual Studio 2010\Code Snippets\Visual C#\My Code Snippets"
+pause
