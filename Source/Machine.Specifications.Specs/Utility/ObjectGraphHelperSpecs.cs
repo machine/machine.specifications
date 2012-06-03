110af0252c6436c3326f413d7eaed5d3b448337c
Richard Oliver <richard@rholiver.co.uk>
2012-06-03 16:26:49 +0100

    Started ObjectGraphHelper

---
+++ Source/Machine.Specifications.Specs/Utility/ObjectGraphHelperSpecs.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Machine.Specifications.Utility;
+
+namespace Machine.Specifications.Specs.Utility
+{
+    [Subject(typeof(ObjectGraphHelper))]
+    public class when_getting_an_object_graph
+    {
+        static object _obj;
+        static IEnumerable<ObjectGraphHelper.Member> _result;
+
+        public class with_property
+        {
+            Establish ctx = () => _obj = new {Property = "value"};
+
+            Because of = () => _result = ObjectGraphHelper.GetGraph(_obj);
+
+            It should_have_property = () => _result.Where(m => m.Name == "Property").ShouldNotBeEmpty();
+            It should_retrieve_value = () => _result.Where(m => m.Name == "Property").Single().ValueGetter().ShouldEqual("value");
+        }
+
+        public class with_field
+        {
+            Establish ctx = () =>
+            {
+                var obj = new ObjectWithField();
+                obj.Field = "value";
+                _obj = obj;
+            };
+
+            Because of = () => _result = ObjectGraphHelper.GetGraph(_obj);
+
+            It should_have_field = () => _result.Where(m => m.Name == "Field").ShouldNotBeEmpty();
+            It should_retrieve_value = () => _result.Where(m => m.Name == "Field").Single().ValueGetter().ShouldEqual("value");
+        }
+
+        public class with_readonly_field
+        {
+            Establish ctx = () => _obj = new ObjectWithReadOnlyField("value");
+
+            Because of = () => _result = ObjectGraphHelper.GetGraph(_obj);
+
+            It should_have_field = () => _result.Where(m => m.Name == "Field").ShouldNotBeEmpty();
+            It should_retrieve_value = () => _result.Where(m => m.Name == "Field").Single().ValueGetter().ShouldEqual("value");
+        }
+
+        
+        public class with_property_and_field
+        {
+            Establish ctx = () =>
+            {
+                var obj = new ObjectWithPropertyAndField();
+                obj.Property = "value1";
+                obj.Field = "value2";
+                _obj = obj;
+            };
+            Because of = () => _result = ObjectGraphHelper.GetGraph(_obj);
+            
+            It should_have_property = () => _result.Where(m => m.Name == "Property").ShouldNotBeEmpty();
+            It should_retrieve_property_value = () => _result.Where(m => m.Name == "Property").Single().ValueGetter().ShouldEqual("value1");
+            It should_have_field = () => _result.Where(m => m.Name == "Field").ShouldNotBeEmpty();
+            It should_retrieve_field_value = () => _result.Where(m => m.Name == "Field").Single().ValueGetter().ShouldEqual("value2");
+        }
+
+    }
+
+    class ObjectWithField
+    {
+        public string Field;
+    }
+
+    class ObjectWithReadOnlyField
+    {
+        public readonly string Field;
+
+        public ObjectWithReadOnlyField(string field)
+        {
+            Field = field;
+        }
+    }
+
+    class ObjectWithPropertyAndField
+    {
+        public string Property { get; set; }   
+        public string Field;
+    }
+}
---
+++ Source/Machine.Specifications/Utility/ObjectGraphHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Machine.Specifications.Utility
+{
+    /// <summary>
+    /// Modified from Steve Wagner's ObjectDiff prototype https://gist.github.com/2841822
+    /// </summary>
+    public static class ObjectGraphHelper
+    {
+        public static IEnumerable<Member> GetGraph(object obj)
+        {
+            var properties = obj.GetType().GetProperties()
+                .Where(p => p.CanRead)
+                .Select(p =>
+                {
+                    var prop = p;
+                    return new Member
+                    {
+                        Name = p.Name,
+                        ValueGetter = () => prop.GetValue(obj, null)
+                    };
+                });
+
+            var fields = obj.GetType().GetFields()
+                .Select(f =>
+                {
+                    var field = f;
+                    return new Member
+                    {
+                        Name = f.Name,
+                        ValueGetter = () => field.GetValue(obj)
+                    };
+                });
+
+            return properties.Concat(fields).OrderBy(m => m.Name);
+        }
+
+        public class Member
+        {
+            public string Name { get; set; }
+
+            public Func<object> ValueGetter { get; set; }
+        }
+    }
+}
