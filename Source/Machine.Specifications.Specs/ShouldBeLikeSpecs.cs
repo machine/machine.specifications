5b3da7cb95d4bf3cc38eecfdb3f8f551b22cac66
Richard Oliver <richard@rholiver.co.uk>
2012-06-04 15:28:52 +0100

    Formatting

--- Source/Machine.Specifications.Specs/ShouldBeLikeSpecs.cs
+++ Source/Machine.Specifications.Specs/ShouldBeLikeSpecs.cs
@@ -5,16 +5,16 @@ namespace Machine.Specifications.Specs
   [Subject(typeof(ShouldExtensionMethods))]
   public class when_asserting_object_is_like_expected_key_values
   {
+    static Exception Exception;
+    static Dummy Obj;
+
+    Establish context = () => { Obj = new Dummy {Prop1 = "test", Prop2 = 2}; };
+
     class Dummy
     {
       public string Prop1 { get; set; }
       public int Prop2 { get; set; }
     }
-
-    static Exception Exception;
-    static Dummy Obj;
-    
-    Establish context = () => { Obj = new Dummy{Prop1 = "test", Prop2 = 2}; };
 
     public class with_correct_key_values
     {
@@ -26,34 +26,32 @@ namespace Machine.Specifications.Specs
     public class with_incorrect_key_value
     {
       Because of = () => { Exception = Catch.Exception(() => Obj.ShouldBeLike(new {Prop1 = "test2", Prop2 = 2})); };
-      
-      It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
-      It should_contain_message = () => Exception.Message.ShouldEqual(
-@"""Prop1"":
+
+      It should_contain_message = () => Exception.Message.ShouldEqual(@"""Prop1"":
   Expected string length 5 but was 4. Strings differ at index 4.
   Expected: ""test2""
   But was:  ""test""
   ---------------^");
+
+      It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
     }
-    
+
     public class with_missing_key
     {
       Because of = () => { Exception = Catch.Exception(() => Obj.ShouldBeLike(new {Prop1 = "test", Prop2 = 2, Prop3 = "other"})); };
-      
-      It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
-      It should_contain_message = () => Exception.Message.ShouldEqual(
-@"""Prop3"":
+
+      It should_contain_message = () => Exception.Message.ShouldEqual(@"""Prop3"":
   Expected: ""other""
   But was:  Not Defined");
+
+      It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
     }
 
     public class with_multiple_errors
     {
       Because of = () => { Exception = Catch.Exception(() => Obj.ShouldBeLike(new {Prop1 = "test2", Prop2 = 3, Prop3 = "other"})); };
-      
-      It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
-      It should_contain_message = () => Exception.Message.ShouldEqual(
-@"""Prop1"":
+
+      It should_contain_message = () => Exception.Message.ShouldEqual(@"""Prop1"":
   Expected string length 5 but was 4. Strings differ at index 4.
   Expected: ""test2""
   But was:  ""test""
@@ -66,107 +64,120 @@ namespace Machine.Specifications.Specs
 ""Prop3"":
   Expected: ""other""
   But was:  Not Defined");
+
+      It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
     }
   }
 
-    [Subject(typeof(ShouldExtensionMethods))]
-    public class when_asserting_object_is_like_expected_array
+  [Subject(typeof(ShouldExtensionMethods))]
+  public class when_asserting_object_is_like_expected_array
+  {
+    static Exception Exception;
+    static object _array;
+
+    public class with_correct_values
     {
-        static Exception Exception;
-        static object _array;
-        
-        public class with_correct_values
-        {
-            Establish ctx = () => _array = new[] {"value1", "value2"};
-            Because of = () => { Exception = Catch.Exception(() => _array.ShouldBeLike(new[] {"value1", "value2"})); };
+      Establish ctx = () => _array = new[] {"value1", "value2"};
+      Because of = () => { Exception = Catch.Exception(() => _array.ShouldBeLike(new[] {"value1", "value2"})); };
 
-            It should_not_throw = () => Exception.ShouldBeNull();
-        }
-        
-        public class with_no_values
-        {
-            Establish ctx = () => _array = new string[] {};
-            Because of = () => { Exception = Catch.Exception(() => _array.ShouldBeLike(new string[] {})); };
+      It should_not_throw = () => Exception.ShouldBeNull();
+    }
 
-            It should_not_throw = () => Exception.ShouldBeNull();
-        }
+    public class with_no_values
+    {
+      Establish ctx = () => _array = new string[] {};
+      Because of = () => { Exception = Catch.Exception(() => _array.ShouldBeLike(new string[] {})); };
 
-        public class with_incorrect_values
-        {
-            Establish ctx = () => _array = new[] {"value1", "value2"};
-            Because of = () => { Exception = Catch.Exception(() => _array.ShouldBeLike(new[] {"value1", "value3"})); };
-            
-      It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
-      It should_contain_message = () => Exception.Message.ShouldEqual(
-@"""[1]"":
+      It should_not_throw = () => Exception.ShouldBeNull();
+    }
+
+    public class with_incorrect_values
+    {
+      Establish ctx = () => _array = new[] {"value1", "value2"};
+      Because of = () => { Exception = Catch.Exception(() => _array.ShouldBeLike(new[] {"value1", "value3"})); };
+
+      It should_contain_message = () => Exception.Message.ShouldEqual(@"""[1]"":
   String lengths are both 6. Strings differ at index 5.
   Expected: ""value3""
   But was:  ""value2""
   ----------------^");
-        }
-        
-        public class with_incorrect_length
-        {
-            Establish ctx = () => _array = new[] {"value1", "value2"};
-            Because of = () => { Exception = Catch.Exception(() => _array.ShouldBeLike(new[] {"value1"})); };
-            
+
       It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
-      It should_contain_message = () => Exception.Message.ShouldEqual(
-@""""":
+    }
+
+    public class with_incorrect_length
+    {
+      Establish ctx = () => _array = new[] {"value1", "value2"};
+      Because of = () => { Exception = Catch.Exception(() => _array.ShouldBeLike(new[] {"value1"})); };
+
+      It should_contain_message = () => Exception.Message.ShouldEqual(@""""":
   Expected: Array length of 1
   But was:  2");
-        }
+
+      It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
+    }
+  }
+
+  [Subject(typeof(ShouldExtensionMethods))]
+  public class when_asserting_object_is_like_expected_nested_object
+  {
+    static Exception Exception;
+    static Dummy _obj = new Dummy {Prop1 = "value1", Prop2 = 10, NestedProp = new NestedDummy {NestedProp1 = 5, NestedProp2 = "value2"}};
+
+    class Dummy
+    {
+      public string Prop1 { get; set; }
+      public int Prop2 { get; set; }
+      public NestedDummy NestedProp { get; set; }
     }
 
-    [Subject(typeof(ShouldExtensionMethods))]
-    public class when_asserting_object_is_like_expected_nested_object
+    class NestedDummy
     {
-        class Dummy
-        {
-            public string Prop1 { get; set; }
-            public int Prop2 { get; set; }
-            public NestedDummy NestedProp { get; set; }
-        }
+      public int NestedProp1 { get; set; }
+      public string NestedProp2 { get; set; }
+    }
 
-        class NestedDummy
-        {
-            public int NestedProp1 { get; set; }
-            public string NestedProp2 { get; set; }
-        }
+    public class with_correct_values
+    {
+      Because of = () => { Exception = Catch.Exception(() => _obj.ShouldBeLike(new {Prop1 = "value1", Prop2 = 10, NestedProp = new {NestedProp1 = 5, NestedProp2 = "value2"}})); };
 
-        static Exception Exception;
-        static Dummy _obj = new Dummy{Prop1 = "value1", Prop2 = 10, NestedProp = new NestedDummy{NestedProp1 = 5, NestedProp2 = "value2"}};
-        
-        public class with_correct_values
-        {
-            Because of = () => { Exception = Catch.Exception(() => _obj.ShouldBeLike(
-                new{Prop1 = "value1", Prop2 = 10, NestedProp = new{NestedProp1 = 5, NestedProp2 = "value2"}})); };
+      It should_not_throw = () => Exception.ShouldBeNull();
+    }
 
-            It should_not_throw = () => Exception.ShouldBeNull();
-        }
-        
-        public class with_incorrect_values
-        {
-            Because of = () => { Exception = Catch.Exception(() => _obj.ShouldBeLike(
-                new{Prop1 = "value1", Prop2 = 10, NestedProp = new{NestedProp1 = 7, NestedProp2 = "value2"}})); };
-            
-      It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
-      It should_contain_message = () => Exception.Message.ShouldEqual(
-@"""NestedProp.NestedProp1"":
+    public class with_incorrect_values
+    {
+      Because of = () => { Exception = Catch.Exception(() => _obj.ShouldBeLike(new {Prop1 = "value1", Prop2 = 10, NestedProp = new {NestedProp1 = 7, NestedProp2 = "value2"}})); };
+
+      It should_contain_message = () => Exception.Message.ShouldEqual(@"""NestedProp.NestedProp1"":
   Expected: [7]
   But was:  [5]");
-        }
-        
-        public class with_missing_value
-        {
-            Because of = () => { Exception = Catch.Exception(() => _obj.ShouldBeLike(
-                new{Prop1 = "value1", Prop2 = 10, NestedProp = new{NestedProp1 = 5, NestedProp2 = "value2", NestedProp3 = "value3"}})); };
-            
+
       It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
-      It should_contain_message = () => Exception.Message.ShouldEqual(
-@"""NestedProp.NestedProp3"":
+    }
+
+    public class with_missing_value
+    {
+      Because of = () =>
+      {
+        Exception = Catch.Exception(() => _obj.ShouldBeLike(
+                                                            new
+                                                            {
+                                                              Prop1 = "value1",
+                                                              Prop2 = 10,
+                                                              NestedProp = new
+                                                              {
+                                                                NestedProp1 = 5,
+                                                                NestedProp2 = "value2",
+                                                                NestedProp3 = "value3"
+                                                              }
+                                                            }));
+      };
+
+      It should_contain_message = () => Exception.Message.ShouldEqual(@"""NestedProp.NestedProp3"":
   Expected: ""value3""
   But was:  Not Defined");
-        }
+
+      It should_throw = () => Exception.ShouldBeOfType<SpecificationException>();
     }
+  }
 }
--- Source/Machine.Specifications/Utility/ObjectGraphHelper.cs
+++ Source/Machine.Specifications/Utility/ObjectGraphHelper.cs
@@ -4,87 +4,87 @@ using System.Linq;
 
 namespace Machine.Specifications.Utility
 {
-    /// <summary>
-    /// Modified from Steve Wagner's ObjectDiff prototype https://gist.github.com/2841822
-    /// </summary>
-    public static class ObjectGraphHelper
+  /// <summary>
+  /// Modified from Steve Wagner's ObjectDiff prototype https://gist.github.com/2841822
+  /// </summary>
+  public static class ObjectGraphHelper
+  {
+    public static INode GetGraph(object obj)
     {
-        public static INode GetGraph(object obj)
-        {
-            var objectType = obj.GetType();
-            if (objectType.IsArray)
-            {
-                return GetArrayNode(obj);
-            }
-            else if(objectType.IsClass && objectType != typeof(string))
-            {
-                return GetKeyValueNode(obj);
-            }
-            else
-            {
-                return new LiteralNode {Value = obj};
-            }
-        }
-
-        static INode GetArrayNode(object obj)
-        {
-            var array = (IEnumerable<object>) obj;
-
-            return new ArrayNode {ValueGetters = array.Select<object, Func<object>>(a => () => a).ToArray()};
-        }
-
-        static INode GetKeyValueNode(object obj)
-        {
-            var properties = obj.GetType().GetProperties()
-                .Where(p => p.CanRead)
-                .Select(p =>
-                {
-                    var prop = p;
-                    return new Member
-                    {
-                        Name = p.Name,
-                        ValueGetter = () => prop.GetValue(obj, null)
-                    };
-                });
-
-            var fields = obj.GetType().GetFields()
-                .Select(f =>
-                {
-                    var field = f;
-                    return new Member
-                    {
-                        Name = f.Name,
-                        ValueGetter = () => field.GetValue(obj)
-                    };
-                });
-
-            return new KeyValueNode {KeyValues = properties.Concat(fields).OrderBy(m => m.Name)};
-        }
-
-        public interface INode
-        {
-        }
-
-        public class ArrayNode : INode
-        {
-            public IEnumerable<Func<object>> ValueGetters { get; set; }
-        }
-
-        public class KeyValueNode : INode
-        {
-            public IEnumerable<Member> KeyValues { get; set; }
-        }
-
-        public class LiteralNode:INode
-        {
-            public object Value { get; set; }
-        }
-
-        public class Member
-        {
-            public string Name { get; set; }
-
-            public Func<object> ValueGetter { get; set; }
-        }
+      var objectType = obj.GetType();
+      if (objectType.IsArray)
+      {
+        return GetArrayNode(obj);
+      }
+      else if (objectType.IsClass && objectType != typeof(string))
+      {
+        return GetKeyValueNode(obj);
+      }
+      else
+      {
+        return new LiteralNode {Value = obj};
+      }
     }
+
+    static INode GetArrayNode(object obj)
+    {
+      var array = (IEnumerable<object>) obj;
+
+      return new ArrayNode {ValueGetters = array.Select<object, Func<object>>(a => () => a).ToArray()};
+    }
+
+    static INode GetKeyValueNode(object obj)
+    {
+      var properties = obj.GetType().GetProperties()
+        .Where(p => p.CanRead)
+        .Select(p =>
+        {
+          var prop = p;
+          return new Member
+          {
+            Name = p.Name,
+            ValueGetter = () => prop.GetValue(obj, null)
+          };
+        });
+
+      var fields = obj.GetType().GetFields()
+        .Select(f =>
+        {
+          var field = f;
+          return new Member
+          {
+            Name = f.Name,
+            ValueGetter = () => field.GetValue(obj)
+          };
+        });
+
+      return new KeyValueNode {KeyValues = properties.Concat(fields).OrderBy(m => m.Name)};
+    }
+
+    public class ArrayNode : INode
+    {
+      public IEnumerable<Func<object>> ValueGetters { get; set; }
+    }
+
+    public interface INode
+    {
+    }
+
+    public class KeyValueNode : INode
+    {
+      public IEnumerable<Member> KeyValues { get; set; }
+    }
+
+    public class LiteralNode : INode
+    {
+      public object Value { get; set; }
+    }
+
+    public class Member
+    {
+      public string Name { get; set; }
+
+      public Func<object> ValueGetter { get; set; }
+    }
+  }
 }
