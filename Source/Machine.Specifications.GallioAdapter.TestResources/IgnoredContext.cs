af1f39d83574cbb41757e8cf89a307021cd5b620
Niels Hanberg <nielshanberg@gmail.com>
2012-08-02 12:10:12 +0200

    Remove and sort usings

--- Source/Machine.Specifications.GallioAdapter.TestResources/IgnoredContext.cs
+++ Source/Machine.Specifications.GallioAdapter.TestResources/IgnoredContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Machine.Specifications.GallioAdapter.TestResources
+﻿namespace Machine.Specifications.GallioAdapter.TestResources
 {
   [Ignore]
   public class ignored_context_spec
