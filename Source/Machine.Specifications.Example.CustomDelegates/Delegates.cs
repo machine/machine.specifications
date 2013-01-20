a055c913693c0f8bc4e25988d5eb318878441db2
Alexander Groß <agross@therightstuff.de>
2013-01-20 13:36:03 +0100

    Adding an example for custom delegates

---
+++ Source/Machine.Specifications.Example.CustomDelegates/Delegates.cs
@@ -0,0 +1,11 @@
+﻿namespace Machine.Specifications.Example.CustomDelegates
+{
+  [DelegateUsage(DelegateUsage.Setup)]
+  public delegate void Given();
+
+  [DelegateUsage(DelegateUsage.Act)]
+  public delegate void When();
+
+  [DelegateUsage(DelegateUsage.Assert)]
+  public delegate void Then();
+}
---
+++ Source/Machine.Specifications.Specs/Runner/CustomDelegatesSpecs.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Machine.Specifications.Example.CustomDelegates;
+
+namespace Machine.Specifications.Specs.Runner
+{
+  public class when_running_specs_with_custom_delegates : running_specs
+  {
+    Because of = 
+      () => runner.RunAssembly(typeof(Account).Assembly);
+
+    It should_run_them_all = 
+      () => listener.SpecCount.ShouldEqual(3);
+  }
+}
