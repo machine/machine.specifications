c57f369557d8c56e3ac83e77f3b0be770ad10a06
Alexander Groß <agross@therightstuff.de>
2013-05-10 09:46:55 +0200

    Fixing installer batch files for dotCover, closes http://youtrack.jetbrains.com/issue/DCVR-4956

--- Source/Runners/Machine.Specifications.dotCoverRunner.2.5/InstallDotCoverRunner.2.5.bat
+++ Source/Runners/Machine.Specifications.dotCoverRunner.2.5/InstallDotCoverRunner.2.5.bat
@@ -1,7 +1,7 @@
-mkdir "%LOCALAPPDATA%\JetBrains\dotCover\v2.5\Plugins" 2> NUL
-mkdir "%LOCALAPPDATA%\JetBrains\dotCover\v2.5\Plugins\mspec" 2> NUL
-copy /y Machine.Specifications.dll "%LOCALAPPDATA%\JetBrains\dotCover\v2.5\Plugins\mspec"
-copy /y Machine.Specifications.pdb "%LOCALAPPDATA%\JetBrains\dotCover\v2.5\Plugins\mspec" > NUL
-copy /y Machine.Specifications.dotCoverRunner.2.5.dll "%LOCALAPPDATA%\JetBrains\dotCover\v2.5\Plugins\mspec"
-copy /y Machine.Specifications.dotCoverRunner.2.5.pdb "%LOCALAPPDATA%\JetBrains\dotCover\v2.5\Plugins\mspec" > NUL
+mkdir "%APPDATA%\JetBrains\dotCover\v2.5\Plugins" 2> NUL
+mkdir "%APPDATA%\JetBrains\dotCover\v2.5\Plugins\mspec" 2> NUL
+copy /y Machine.Specifications.dll "%APPDATA%\JetBrains\dotCover\v2.5\Plugins\mspec"
+copy /y Machine.Specifications.pdb "%APPDATA%\JetBrains\dotCover\v2.5\Plugins\mspec" > NUL
+copy /y Machine.Specifications.dotCoverRunner.2.5.dll "%APPDATA%\JetBrains\dotCover\v2.5\Plugins\mspec"
+copy /y Machine.Specifications.dotCoverRunner.2.5.pdb "%APPDATA%\JetBrains\dotCover\v2.5\Plugins\mspec" > NUL
 pause
