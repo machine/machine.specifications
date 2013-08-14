b75f5a67a4481b1a883125ecaa5475a9df775c59
Alexander Groß <agross@therightstuff.de>
2013-08-14 09:15:55 +0200

    Improved banking specs

--- Source/Examples/Example/BankingSpecs.cs
+++ Source/Examples/Example/BankingSpecs.cs
@@ -8,39 +8,45 @@ namespace Example
   public class when_transferring_between_two_accounts
     : AccountSpecs
   {
-    Because of = () =>
-      fromAccount.Transfer(1m, toAccount);
+    Establish context = () =>
+    {
+      fromAccount = new Account {Balance = 1m};
+      toAccount = new Account {Balance = 1m};
+    };
 
-    It should_debit_the_from_account_by_the_amount_transferred = () =>
-      fromAccount.Balance.ShouldEqual(0m);
+    Because of =
+      () => fromAccount.Transfer(1m, toAccount);
 
-    It should_credit_the_to_account_by_the_amount_transferred = () =>
-      toAccount.Balance.ShouldEqual(2m);
+    It should_debit_the_from_account_by_the_amount_transferred =
+      () => fromAccount.Balance.ShouldEqual(0m);
+
+    It should_credit_the_to_account_by_the_amount_transferred =
+      () => toAccount.Balance.ShouldEqual(2m);
   }
 
-  [Subject(typeof(Account), "Funds transfer"), Tags("failure")]
+  [Subject(typeof(Account), "Funds transfer")]
+  [Tags("failure")]
   public class when_transferring_an_amount_larger_than_the_balance_of_the_from_account
     : AccountSpecs
   {
     static Exception exception;
-    Because of =()=>
-      exception = Catch.Exception(()=>fromAccount.Transfer(2m, toAccount));
 
-    It should_not_allow_the_transfer =()=>
-      exception.ShouldBeOfType<Exception>();
+    Establish context = () =>
+    {
+      fromAccount = new Account {Balance = 1m};
+      toAccount = new Account {Balance = 1m};
+    };
+
+    Because of =
+      () => exception = Catch.Exception(() => fromAccount.Transfer(2m, toAccount));
+
+    It should_not_allow_the_transfer =
+      () => exception.ShouldBeOfType<Exception>();
   }
-
-  public class failure {}
 
   public abstract class AccountSpecs
   {
     protected static Account fromAccount;
     protected static Account toAccount;
-
-    Establish context =()=>
-    {
-      fromAccount = new Account {Balance = 1m};
-      toAccount = new Account {Balance = 1m};
-    };
   }
 }
