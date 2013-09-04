ab38cc4e521e5166c446537d603a8624af00ce14
Alexander Groß <agross@therightstuff.de>
2013-09-04 18:19:21 +0200

    Improved grouping of build messages in TeamCity

--- Tools/Rake/task.rb
+++ Tools/Rake/task.rb
@@ -1,8 +1,16 @@
 class Rake::Task
-	old_execute = self.instance_method(:execute)
-	
-	define_method(:execute) do |args|
-		puts "\n[#{name}]\n"
-		old_execute.bind(self).call(args)
-	end
+  old_execute = self.instance_method(:execute)
+
+  define_method(:execute) do |args|
+    TeamCity.block_opened({ :name => name })
+    TeamCity.progress_start name
+
+    begin
+      puts "\n[#{name}]\n"
+      old_execute.bind(self).call(args)
+    ensure
+      TeamCity.progress_finish name
+      TeamCity.block_closed({ :name => name })
+    end
+  end
 end
