a560e3ad19b8a9cc763ceff5248c08f3664b939b
Alexander Groß <agross@therightstuff.de>
2013-09-04 18:07:12 +0200

    Aligning the layout of the NuGet package and the ZIP distribution

--- rakefile.rb
+++ rakefile.rb
@@ -225,70 +225,84 @@ namespace :package do
       .exclude("#{root}/**/*.InstallState") \
       .exclude("#{root}/Generation") \
       .exclude("#{root}/Tests") \
-      .exclude("#{root}/NuGet")
+      .exclude("#{root}/NuGet") \
+      .exclude("#{root}/Package")
+  end
+
+  def copy_distributed_files_to(destination)
+    mkdir_p destination
+
+    %w(License.txt History.txt).each { |f| cp f, destination }
+
+    net_20_framework_files(configatron.out_dir).copy_hierarchy \
+      :source_dir => configatron.out_dir,
+      :target_dir => "#{destination}/lib/net20"
+
+    net_40_framework_files(configatron.out_dir).copy_hierarchy \
+      :source_dir => configatron.out_dir,
+      :target_dir => "#{destination}/lib/net40"
+
+    packaged_files(configatron.out_dir).copy_hierarchy \
+      :source_dir => configatron.out_dir,
+      :target_dir => "#{destination}/tools"
   end
 
   desc "Package build artifacts as a zip file"
   task :zip => [ 'build:rebuild', 'tests:run', 'specs:run' ] do
     rm_f configatron.zip.package
 
-    %w(License.txt History.txt).each { |f| cp f, configatron.out_dir }
+    copy_distributed_files_to "#{configatron.out_dir}/Package"
 
     sz = SevenZip.new \
       :tool => 'Tools/7-Zip/7za.exe',
       :zip_name => configatron.zip.package
 
-    Dir.chdir(configatron.out_dir) do
-      sz.zip :files => packaged_files
+    Dir.chdir("#{configatron.out_dir}/Package") do
+      sz.zip :files => FileList.new("**/*")
     end
   end
 
-  def create_package
-    opts = ["pack", "mspec.nuspec",
-      "-BasePath", "#{configatron.out_dir}NuGet".gsub(/\//, '\\'),
-      "-OutputDirectory", configatron.nuget.package.dirname]
+  def create_package_from(dir)
+    dir = dir.gsub(%r|/|, '\\').gsub(%r|\\\\|, '\\')
 
-    sh ".nuget/NuGet.exe", *(opts)
+    opts = %W(
+      .nuget/NuGet.exe pack mspec.nuspec
+      -BasePath #{dir}
+      -OutputDirectory #{configatron.nuget.package.dirname}
+      )
+
+    sh(*opts)
   end
 
   namespace :nuget do
     desc "Package build artifacts as a NuGet package and a symbols package"
     task :create => :zip do
-      net_20_framework_files(configatron.out_dir).copy_hierarchy \
-        :source_dir => configatron.out_dir,
-        :target_dir => "#{configatron.out_dir}NuGet/lib/net20"
 
-      net_40_framework_files(configatron.out_dir).copy_hierarchy \
-        :source_dir => configatron.out_dir,
-        :target_dir => "#{configatron.out_dir}NuGet/lib/net40"
-
+      copy_distributed_files_to "#{configatron.out_dir}/NuGet"
       source_files('Source').copy_hierarchy \
         :source_dir => 'Source',
-        :target_dir => "#{configatron.out_dir}NuGet/src/"
+        :target_dir => "#{configatron.out_dir}/NuGet/src/"
 
-      packaged_files(configatron.out_dir).copy_hierarchy \
-        :source_dir => configatron.out_dir,
-        :target_dir => "#{configatron.out_dir}NuGet/tools/"
+      cp 'install.ps1', "#{configatron.out_dir}/NuGet/tools"
 
-      cp 'install.ps1', "#{configatron.out_dir}NuGet/tools/"
-
-      create_package
+      create_package_from "#{configatron.out_dir}/NuGet"
       mv configatron.nuget.package, configatron.nuget.package.pathmap('%X.symbols%x')
 
-      FileList["#{configatron.out_dir}NuGet/src/", "#{configatron.out_dir}NuGet/**/*.pdb"].each { |f| rm_rf f }
-      create_package
+      FileList["#{configatron.out_dir}/NuGet/src/", "#{configatron.out_dir}/NuGet/**/*.pdb"].each { |f| rm_rf f }
+      create_package_from "#{configatron.out_dir}/NuGet"
     end
 
     desc "Publishes the NuGet package"
     task :publish do
       raise "NuGet access key is missing, cannot publish" if configatron.nuget.key.nil?
 
-      opts = ["push",
-        configatron.nuget.package,
-        configatron.nuget.key,
-        { :verbose => false }]
+      opts = %W(
+        .nuget/NuGet.exe push
+        #{configatron.nuget.package}
+        #{configatron.nuget.key}
+      ) << { :verbose => false }
 
-      sh ".nuget/NuGet.exe", *(opts) do |ok, status|
+      sh(*opts) do |ok, status|
         ok or fail "Command failed with status (#{status.exitstatus})"
       end
     end
