c2e17a3afb9c1a10723b06690991c06604356cdc
Daniel Marbach <daniel.marbach@openplace.net>
2014-01-23 22:57:01 +0100

    Catch.Only with throwingAction, closes #205

--- Source/Machine.Specifications.Specs/CatchSpecs.cs
+++ Source/Machine.Specifications.Specs/CatchSpecs.cs
@@ -81,4 +81,51 @@ namespace Machine.Specifications.Specs
         () => Result.Should().BeNull();
     }
   }
+
+  [Subject(typeof(Catch))]
+  public class when_calling_Catch_Only_with_an_Action
+  {
+    [Subject(typeof(Catch))]
+    public class with_a_throwing_Action_which_matches_exception_to_be_caught
+    {
+      static ArgumentException AnException;
+      static Exception Result;
+
+      Establish context = () => { AnException = new ArgumentException(); };
+
+      Because of = () => { Result = Catch.Only<ArgumentException>(() => { throw AnException; }); };
+
+      It should_return_the_same_exception =
+        () => Result.Should().BeSameAs(AnException);
+    }
+
+    [Subject(typeof(Catch))]
+    public class with_a_throwing_Action_which_doesnt_match_exception_to_be_caught
+    {
+      static ArgumentException AnException;
+      static Exception Result;
+
+      Establish context = () => { AnException = new ArgumentException(); };
+
+      Because of = () => { Result = Catch.Exception(() => Catch.Only<InvalidOperationException>(() => { throw AnException; })); };
+
+      It should_return_the_same_exception =
+        () => Result.Should().BeSameAs(AnException);
+    }
+
+    [Subject(typeof(Catch))]
+    public class with_a_non_throwing_Action
+    {
+      static string ActionSideEffect;
+      static Exception Result;
+
+      Because of = () => { Result = Catch.Only<ArgumentException>((() => { ActionSideEffect = "hi"; })); };
+
+      It should_access_the_propety =
+        () => ActionSideEffect.Should().Be("hi");
+
+      It should_return_null =
+        () => Result.Should().BeNull();
+    }
+  }
 }
