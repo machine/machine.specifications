0d5af3df9a3c7970e4a4d2e7e4474c61bbff2aa0
Daniel Marbach <daniel.marbach@openplace.net>
2014-02-17 23:15:46 +0100

    Removed IgnoreAttribute constructor

--- Source/Machine.Specifications/IgnoreAttribute.cs
+++ Source/Machine.Specifications/IgnoreAttribute.cs
@@ -5,11 +5,6 @@ namespace Machine.Specifications
   [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
   public class IgnoreAttribute : Attribute
   {
-    [ObsoleteEx(Message = "Please specify the reason for ignoring the specification", RemoveInVersion = "0.8", TreatAsErrorFromVersion = "0.7")]
-    public IgnoreAttribute()
-    {
-    }
-
     public IgnoreAttribute(string reason)
     {
       Reason = reason;
