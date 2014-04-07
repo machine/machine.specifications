bf37b8d893ac67a09edb0533b6032b644692b7fa
Daniel Marbach <daniel.marbach@openplace.net>
2014-04-07 16:54:48 +0200

    Renamed dotCoverRunner/Provider to dotCover.Runner/Provider
    Renamed ResharperRunner/Provider to Resharper.Runner/Provider

--- Source/Runners/Machine.Specifications.ReSharper.Provider/InstallResharperRunner.8.1.bat
+++ Source/Runners/Machine.Specifications.ReSharper.Provider/InstallResharperRunner.8.1.bat
@@ -5,6 +5,10 @@ mkdir "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins" 2> NUL
 mkdir "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec" 2> NUL
 copy /y Machine.Specifications.dll "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec"
 copy /y Machine.Specifications.pdb "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec" > NUL
-copy /y Machine.Specifications.ReSharperRunner.8.1.dll "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec"
-copy /y Machine.Specifications.ReSharperRunner.8.1.pdb "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec" > NUL
+copy /y Machine.Specifications.ReSharper.Runner.8.1.dll "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec"
+copy /y Machine.Specifications.ReSharper.Runner.8.1.pdb "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec" > NUL
+copy /y Machine.Specifications.ReSharper.Provider.8.1.dll "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec"
+copy /y Machine.Specifications.ReSharper.Provider.8.1.pdb "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec" > NUL
+copy /y Machine.Specifications.Runner.Utility.dll "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec"
+copy /y Machine.Specifications.Runner.Utility.pdb "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec" > NUL
 pause
--- Source/Runners/Machine.Specifications.ReSharper.Runner/InstallResharperRunner.8.0.bat
+++ Source/Runners/Machine.Specifications.ReSharper.Runner/InstallResharperRunner.8.0.bat
@@ -5,10 +5,10 @@ mkdir "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins" 2> NUL
 mkdir "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec" 2> NUL
 copy /y Machine.Specifications.dll "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec"
 copy /y Machine.Specifications.pdb "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec" > NUL
-copy /y Machine.Specifications.ReSharperRunner.8.0.dll "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec"
-copy /y Machine.Specifications.ReSharperRunner.8.0.pdb "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec" > NUL
-copy /y Machine.Specifications.ReSharperProvider.8.0.dll "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec"
-copy /y Machine.Specifications.ReSharperProvider.8.0.pdb "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec" > NUL
+copy /y Machine.Specifications.ReSharper.Runner.8.0.dll "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec"
+copy /y Machine.Specifications.ReSharper.Runner.8.0.pdb "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec" > NUL
+copy /y Machine.Specifications.ReSharper.Provider.8.0.dll "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec"
+copy /y Machine.Specifications.ReSharper.Provider.8.0.pdb "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec" > NUL
 copy /y Machine.Specifications.Runner.Utility.dll "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec"
 copy /y Machine.Specifications.Runner.Utility.pdb "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec" > NUL
 pause
