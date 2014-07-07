688cd1c9ca7cdce6b0d0751b7d351b8b31967388
Daniel Marbach <daniel.marbach@openplace.net>
2014-07-07 23:13:14 +0200

    Non-project files are ignored, closes #238

--- Source/Runners/Machine.Specifications.ReSharperRunner.7.1/Explorers/MspecTestFileExplorer.cs
+++ Source/Runners/Machine.Specifications.ReSharperRunner.7.1/Explorers/MspecTestFileExplorer.cs
@@ -25,6 +25,13 @@ namespace Machine.Specifications.ReSharperRunner.Explorers
 #if RESHARPER_81
     public void ExploreFile(IFile psiFile, UnitTestElementLocationConsumer consumer, Func<bool> interrupted)
     {
+      if (psiFile == null)
+          throw new ArgumentNullException("psiFile");
+
+      var project = psiFile.GetProject();
+      if (project == null)
+          return;
+
       if ((psiFile.Language.Name == "CSHARP") || (psiFile.Language.Name == "VBASIC"))
       {
         psiFile.ProcessDescendants(new FileExplorer(_provider, _factories, psiFile, consumer, interrupted));
@@ -34,6 +41,13 @@ namespace Machine.Specifications.ReSharperRunner.Explorers
     public void ExploreFile(IFile psiFile, UnitTestElementLocationConsumer consumer, CheckForInterrupt interrupted)
     {
       Func<bool> interruptedFunc = () => interrupted();
+
+      if (psiFile == null)
+          throw new ArgumentNullException("psiFile");
+
+      var project = psiFile.GetProject();
+      if (project == null)
+          return;
 
        if ((psiFile.Language.Name == "CSHARP") || (psiFile.Language.Name == "VBASIC"))
       {
