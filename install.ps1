16b963c259657e88dfbb8161cb60f29e56d154be
dmarbach <daniel.marbach@nsgroup.ch>
2014-12-23 21:30:31 +0100

    Signed version nuspec (duplicated spec)

---
+++ install.ps1
@@ -0,0 +1,3 @@
+param($installPath, $toolsPath, $package, $project)
+
+Write-Warning "This is the unsigned version of Machine.Specifications. Use 'Install-Package Machine.Specifications-Signed' to install the signed version."
