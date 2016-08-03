ffd8d968f8f4b530203c04f38e1a218c201d1386
Ivan Zlatev <ivan@ivanz.com>
2016-08-03 17:00:10 +0100

    Bump assembly version and apply assembly informational version.

--- Source/Machine.Specifications/Properties/AssemblyInfo.cs
+++ Source/Machine.Specifications/Properties/AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Security;
 [assembly : AssemblyCulture("")]
 
 [assembly: AssemblyVersion("0.9.3.0")]
+[assembly: AssemblyInformationalVersion("1.0.*")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
