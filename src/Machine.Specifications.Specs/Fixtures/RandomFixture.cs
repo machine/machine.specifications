acbf0d14381edd27c99b1f2b546826c43121a77b
Robert Coltheart <robert.coltheart@gmail.com>
2021-02-09 22:12:06 +0800

    Add async catch method

--- src/Machine.Specifications.Specs/Fixtures/RandomFixture.cs
+++ src/Machine.Specifications.Specs/Fixtures/RandomFixture.cs
@@ -1092,6 +1092,39 @@ namespace Machine.Specifications
         };
     }
 
+    public class AsyncSpecificationsValueTask
+    {
+        public static int establish_value;
+
+        public static int because_value;
+
+        public static int async_it_value;
+
+        public static int sync_it_value;
+
+        public static int cleanup_value;
+
+        public static ValueTask<int> Test()
+        {
+            return new ValueTask<int>(10);
+        }
+
+        Establish context = async () =>
+            establish_value = await Test();
+
+        Because of = async () =>
+            because_value = await Test();
+
+        It should_invoke_sync = () =>
+            sync_it_value = Test().Result;
+
+        It should_invoke_async = async () =>
+            async_it_value = await Test();
+
+        Cleanup after = async () =>
+            cleanup_value = await Test();
+    }
+
     public class AsyncSpecificationsWithExceptions
     {
         Because of = async () =>
