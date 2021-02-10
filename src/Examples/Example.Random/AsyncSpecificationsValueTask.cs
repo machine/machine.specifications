8d930f83fb4c63d7f890426d596d60d7eff6d348
Robert Coltheart <13191652+robertcoltheart@users.noreply.github.com>
2021-02-10 21:58:40 +0800

    Feature/async catch backport (#431)
    * Add async catch method (#430)
    
    * Only 4.5 and netstandard supported

---
+++ src/Examples/Example.Random/AsyncSpecificationsValueTask.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Machine.Specifications;
+
+#if !NET45
+namespace Example.Random
+{
+    public class AsyncSpecificationsValueTask
+    {
+        public static int establish_value;
+
+        public static int because_value;
+
+        public static int async_it_value;
+
+        public static int sync_it_value;
+
+        public static int cleanup_value;
+
+        public static ValueTask<int> Test()
+        {
+            return new ValueTask<int>(10);
+        }
+
+        Establish context = async () =>
+            establish_value = await Test();
+
+        Because of = async () =>
+            because_value = await Test();
+
+        It should_invoke_sync = () =>
+            sync_it_value = Test().Result;
+
+        It should_invoke_async = async () =>
+            async_it_value = await Test();
+
+        Cleanup after = async () =>
+            cleanup_value = await Test();
+    }
+}
+#endif
--- src/Machine.Specifications/Catch.cs
+++ src/Machine.Specifications/Catch.cs
@@ -1,4 +1,7 @@
 using System;
+#if !NET35 && !NET40
+using System.Threading.Tasks;
+#endif
 
 namespace Machine.Specifications
 {
@@ -6,25 +9,63 @@ namespace Machine.Specifications
     {
         public static Exception Exception(Action throwingAction)
         {
-            return Only<Exception>(throwingAction);
-        }
-
-        public static Exception Exception<T>(Func<T> throwingFunc)
-        {
             try
             {
-                throwingFunc();
+                throwingAction();
             }
-            catch (Exception exception)
+            catch (Exception ex)
             {
-                return exception;
+                return ex;
             }
 
             return null;
         }
 
+        public static Exception Exception<T>(Func<T> throwingFunc)
+        {
+#if !NET35 && !NET40
+            Task task;
+#endif
+            try
+            {
+                var result = throwingFunc();
+#if !NET35 && !NET40
+                task = result as Task;
+#endif
+            }
+            catch (Exception exception)
+            {
+                return exception;
+            }
+#if !NET35 && !NET40
+            if (task != null)
+            {
+                throw new InvalidOperationException("You must use Catch.ExceptionAsync for async methods");
+            }
+#endif
+            return null;
+        }
+
+#if !NET35 && !NET40
+        public static async Task<Exception> ExceptionAsync(Func<Task> throwingAction)
+        {
+            Exception exception = null;
+
+            try
+            {
+                await throwingAction();
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            return exception;
+        }
+#endif
+
         public static TException Only<TException>(Action throwingAction)
-          where TException : Exception
+            where TException : Exception
         {
             try
             {
